bf1cf789c6262f0d6a6d246c8c62c11eb99cdd4d
abe312 <abhineet312@gmail.com>
2019-10-11 16:39:43 +0530

    jwt done: login and current post routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 // load user model
 const User = require('../../models/User');
@@ -54,7 +57,7 @@ router.post('/register', async (req, res) => {
 
 /*
   @route    GET api/users/login
-  @desc     login user
+  @desc     login user (returning jwt )
   @access   public
 */
 router.post('/login', async (req, res) => {
@@ -65,11 +68,41 @@ router.post('/login', async (req, res) => {
   bcrypt.compare(password, user.password, (err, isMatch) => {
     console.log(isMatch);
     if (isMatch) {
-      res.json({ msg: 'success' });
+      // res.json({ msg: 'success' });
+      // user matched
+
+      const payload = { id: user.id, name: user.name, avatar: user.avatar }; // create jwt payload
+
+      // sign token
+      jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
+        res.json({
+          success: true,
+          token: 'Bearer ' + token,
+        });
+        console.log('here login', token);
+      });
     } else {
       return res.status(400).json({ password: 'password incorrect' });
     }
   });
 });
+
+/*
+  @route    GET api/users/current
+  @desc     return current user
+  @access   private
+*/
+router.post(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { id, name, email } = req.user;
+    res.json({
+      id,
+      name,
+      email,
+    });
+  },
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const passport = require('passport');
 
 const app = express();
 
@@ -21,7 +22,11 @@ mongoose
   .then(() => console.log('mongoDB connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('hello world'));
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+require('./config/passport')(passport);
 
 // use routes
 app.use('/api/users', users);
