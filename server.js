b594f24ccfc0fc35dd91444b9646af354714a408
abe312 <abhineet312@gmail.com>
2019-11-16 19:11:11 +0530

    removed build folder

--- server.js
+++ server.js
@@ -34,10 +34,11 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// server static assets if in production
+// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  // set static folder
+  // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
